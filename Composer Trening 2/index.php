a1aa0cfa5969bb61d504a36e5079da6f80c9d3fa
Redivv <j.rajca45@gmail.com>
2019-06-18 20:29:17 +0200

    Composer 2

---
+++ Composer Trening 2/index.php
@@ -0,0 +1,5 @@
+<?php
+    require 'vendor/autoload.php';
+    $animal = new Dafuq\Animal();
+    echo $animal->roar();
+?>
---
+++ Composer Trening 2/src/Animal.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dafuq;
+
+class Animal {
+    public function roar()
+    {
+        return 'A lot';
+    }
+}
+
+?>
