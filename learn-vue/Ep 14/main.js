6ca599a5d738e6e8df072444df98e5ab1ac73681
redivv <j.rajca45@gmail.com>
2020-03-24 17:34:11 +0100

    ewd

--- learn-vue/Ep 14/main.js
+++ learn-vue/Ep 14/main.js
@@ -1,25 +1,37 @@
+window.Event = new Vue();
+
 Vue.component('modal',{
     template: `
-    <div class="modal is-active">
-        <div class="modal-background"></div>
-        <div class="modal-card">
-        <header class="modal-card-head">
-            <p class="modal-card-title">
-                <slot name="header"></slot>
-            </p>
-            <button class="delete" aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-            <slot name="mainContent"></slot>
-        </section>
-        <footer class="modal-card-foot">
-            <slot name="footer">
-                <button class="button is-success">Yas Bitch</button>
-            </slot>
-        </footer>
+    <div>
+        <div class="modal" :class="{'is-active': visibleModal}">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+            <header class="modal-card-head">
+                <p class="modal-card-title">
+                    <slot name="header"></slot>
+                </p>
+                <button @click="visibleModal = false" class="delete" aria-label="close"></button>
+            </header>
+            <section class="modal-card-body">
+                <slot name="mainContent"></slot>
+            </section>
+            <footer class="modal-card-foot">
+                <slot name="footer">
+                    <button class="button is-success">Yas Bitch</button>
+                </slot>
+            </footer>
+            </div>
         </div>
+
+        <button @click="visibleModal = true"> Open Modal </button>
     </div>
     `,
+
+    data(){
+        return {
+            visibleModal: false,
+        };
+    }
 });
 
 let app = new Vue({
