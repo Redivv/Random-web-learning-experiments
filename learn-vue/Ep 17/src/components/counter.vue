0bba8ef48e90b9d4b3d18cd83c057ba6c16fa0bf
redivv <j.rajca45@gmail.com>
2020-03-25 15:55:12 +0100

    LARAVEL BABY

---
+++ learn-vue/Ep 17/src/components/counter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Count is: {{ count }}</h2>
+
+        <button @click="count += 1">Go up</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        };
+    },
+}
+</script>
