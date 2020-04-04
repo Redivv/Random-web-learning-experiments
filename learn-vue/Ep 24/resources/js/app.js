3d346a4f5efa1e41b3a54eef709df901bc0fc42c
redivv <j.rajca45@gmail.com>
2020-04-04 19:25:00 +0200

    d

--- learn-vue/Ep 24/resources/js/app.js
+++ learn-vue/Ep 24/resources/js/app.js
@@ -1,1 +1,54 @@
-require('./bootstrap');
+import Vue from "vue";
+
+Vue.component('coupon',{
+    props: ['code'],
+    template: `
+        <input type="text" :value="code" @input="updateCode($event.target.value)" ref="input">
+    `,
+
+    data(){
+        return {
+            invalids: ['ALLFREE','KEKPENIS']
+        };
+    },
+
+    methods: {
+        updateCode(code){
+            if (this.invalids.includes(code)) {
+                alert("frajer");
+
+                this.$refs.input.value = code = '';
+            }
+
+            this.$emit('input',code);
+        }
+    },
+});
+
+
+new Vue({
+    el: "#app",
+
+
+    data: {
+        code: "FREE"
+    }
+});
+
+let userData = {
+    user:{
+        name: "kek",
+    }
+};
+
+new Vue({
+   el: "#one",
+   
+   data: userData
+});
+
+new Vue ({
+    el: "#two",
+
+    data: userData
+});
